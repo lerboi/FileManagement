8092728e44f20a16e3ed1eb7f985054264df6b67
lerboi <leroyzzng@gmail.com>
2025-09-02 02:39:18 +0800

    edited supabase url .env

--- src/app/api/auth/callback/route.js
+++ src/app/api/auth/callback/route.js
@@ -3,8 +3,8 @@ import { createClient } from '@supabase/supabase-js'
 import { createClientComponentClient, createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+const supabaseUrl = process.env.SUPABASE_URL
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY
 
 // For client-side usage
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
